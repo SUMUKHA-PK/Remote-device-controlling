fe1a980a3e63ed05beb4675e3501bd1bba905751
SUMUKHA-PK <sumukhapk46@gmail.com>
2018-06-27 11:53:22 +0530

    Minor changes

--- server/app.js
+++ server/app.js
@@ -4,11 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var index = require('./routes/index');
 
 var app = express();
-
+var io = require('socket.io')(app);
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -16,8 +15,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', index);
+app.get('/socket',function(req,res,next){
+  res.sendFile(__dirname + '/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
