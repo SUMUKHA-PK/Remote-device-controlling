9cfc7fe078b4ba0be664b029797095c18ca368c9
26prajval98 <26prajval98@gmail.com>
2018-06-28 20:56:00 +0530

    get extension

--- server/blocks/file_recogniser/index.js
+++ server/blocks/file_recogniser/index.js
@@ -1,3 +1,17 @@
-module.exports = function(file){
-    
+const fs = require('fs')
+
+module.exports = function (file) {
+
+    var data = JSON.parse(fs.readFileSync(__dirname + '/formats.json', 'utf8'))
+
+    //get file extension
+    var ext = file.slice(file.lastIndexOf('.')+1)
+
+    for (var key in data){
+        if(data[key].indexOf(ext)+1){
+            return key
+        }
+    }
+
+    return null
 }
