fbab4ae84e80c1b3331d21dd775b4abca9a83515
26prajval98 <26prajval98@gmail.com>
2018-06-28 21:23:32 +0530

    Done

--- server/blocks/FILERUN/index.js
+++ server/blocks/FILERUN/index.js
@@ -4,9 +4,13 @@ var runFileF = require('./application_runner')
 
 module.exports = function(file){
     
+    // get file extension (mkv)
     var getExtension = getExtensionF(file)
+
+    // get path ("C:/Program Files (x86)/Windows Media Player/wmplayer.exe")
     var getCommand = getCommandF(getExtension)
 
+    // Run the file
     if(getCommand === null){
         runFileF([file])
     }
