d9f0ba7823aa3f015c5f4fbf7fdde6def2047d3e
26prajval98 <26prajval98@gmail.com>
2018-06-28 21:01:35 +0530

    don't put unecessary code on QA or master

--- server/app.js
+++ server/app.js
@@ -16,9 +16,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
-app.get('/socket',function(req,res,next){
-  res.sendFile(__dirname + '/index.html');
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
