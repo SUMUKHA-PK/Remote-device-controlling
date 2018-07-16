ff68e5ca95aadf9f7644ea8f5a7ff793efbc949d
26prajval98 <26prajval98@gmail.com>
2018-07-16 23:04:46 +0530

    kasmd

--- server/blocks/models/Folder.js
+++ server/blocks/models/Folder.js
@@ -1,16 +1,16 @@
 class Folder {
     //This is called by /server/change_directory/index.js
-    constructor(name,path){
+    constructor(name, path) {
         this.name = name;
         this.path = path;
     }
 }
 
-Folder.prototype.getName = function(){
+Folder.prototype.getName = function () {
     return this.name
 }
 
-Folder.prototype.getPath = function(){
+Folder.prototype.getPath = function () {
     return this.path
 }
 
